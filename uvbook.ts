304f65888771e9554ac745deed14b0873713ac11
Febzey <79516038+Febzey@users.noreply.github.com>
2023-04-13 13:14:54 -0500

    Update uvbook.ts
    changed spelling.

--- uvbook.ts
+++ uvbook.ts
@@ -1,6 +1,6 @@
 import type Bot from '../structure/mineflayer/Bot.js';
 
-const bookMessage = "You can write in the People of UV book today! Every player gets to write 1 page a year. Every month Iberium choose's one random day where you can write. At the end of the year, copies of the original will be distributed as a limited item for ~1 month!
+const bookMessage = "You can write in the People of UV book today! Every player gets to write 1 page a year. Every month Iberium choose's one random day where you can write. At the end of the year, copies of original will be distributed as a limited item for ~1m!
 "
 
 export default {
